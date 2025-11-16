c357020f01ad40ceb661d5ed34b4e0938b2c4fb9
obinxyz77 <robinasqi6@gmail.com>
2025-11-16 17:29:00 +0700

    Add initial settings configuration for bot

---
+++ settings.js
@@ -0,0 +1,18 @@
+//========= bot tele v2 =========//
+const settings = {
+  token: 'Ganti dengan token punya kamu',
+  adminId: '8305321213',
+  
+
+//========= botzmarket =========//
+  pp: 'https://files.catbox.moe/eu0sp3.jpg',
+  urladmin: 'https://t.me/botzmarket95',
+  domain: 'ganti dengan domain panel kamu',
+  plta: 'ptla',
+  pltc: 'ptlc',
+  loc: '1',
+  eggs: '15'
+};
+//========= botzmarket =========//
+
+module.exports = settings; 
